218ce19f50acb01804c56a39640df2801863825a
Diogros <henriquediogo730@gmail.com>
2026-06-26 16:46:37 -0300

    tree

--- frontend/script.js
+++ frontend/script.js
@@ -7,20 +7,16 @@ $(document).ready(function() {
         const pet = $('#pet').val().trim();
         const servico = $('#servico').val();
 
-        if(tutor === "" || pet === "" || servico === "") {
-            alert("Por favor, preencha todos os campos.");
-            return;
-        }
+        if(tutor === "" || pet === "" || servico === "") return;
 
-        const horaCriacao = Date.now();
-
+        const agora = Date.now();
         const novoCardHTML = `
-            <div class="card" data-time="${horaCriacao}">
+            <div class="card" data-time="${agora}">
                 <strong>🐶 Pet: ${pet}</strong>
                 <span>👤 Tutor: ${tutor}</span>
                 <span>✂️ Serviço: ${servico}</span>
                 <div class="card-actions">
-                    <button class="btn-avancar">Avançar Status ➔</button>
+                    <button class="btn-avancar">Avançar ➔</button>
                 </div>
             </div>
         `;
@@ -37,9 +33,12 @@ $(document).ready(function() {
         const colunaAtual = $card.closest('.kanban-coluna').attr('id');
 
         $card.fadeOut(400, function() {
+            $card.removeClass('alerta-tempo');
+            
+            $card.attr('data-time', Date.now()); 
             
             if(colunaAtual === 'coluna-espera') {
-                $card.removeClass('alerta-tempo').addClass('atendimento');
+                $card.addClass('atendimento');
                 $('#coluna-banho .cards-container').append($card);
             
             } else if (colunaAtual === 'coluna-banho') {
@@ -52,16 +51,19 @@ $(document).ready(function() {
         });
     });
 
-    setInterval(function() {
-        const agora = Date.now();
-        
-        $('#coluna-espera .card').each(function() {
-            const tempoCriacao = $(this).data('time');
-            const diferenca = agora - tempoCriacao;
-
-            if(diferenca > 10000) { 
+    function verificarAtrasos(seletorColuna, tempoLimiteMs) {
+        $(seletorColuna + ' .card').each(function() {
+            const tempoEntrada = parseInt($(this).attr('data-time'));
+            const agora = Date.now();
+            
+            if((agora - tempoEntrada) > tempoLimiteMs) { 
                 $(this).addClass('alerta-tempo'); 
             }
         });
+    }
+
+    setInterval(function() {
+        verificarAtrasos('#coluna-espera', 10000);
+        verificarAtrasos('#coluna-banho', 15000);
     }, 2000);
 });
